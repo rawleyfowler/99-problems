643b55a75176ba892728ffb32b87196d20d19e23
Rawley Fowler <rawleyfowler@gmail.com>
2022-08-02 20:12:24 -0600

    DAY 2

--- 7_flatten_a_nested_list.ml
+++ 7_flatten_a_nested_list.ml
@@ -1,3 +1,21 @@
 type 'a node =
   | One of 'a
   | Many of 'a node list;;
+
+(* My solution *)
+let rec flatten lst =
+  let rec aux acc = function
+    | [] -> acc
+    | Many a :: t -> aux (acc @ (flatten a)) t
+    | One a :: t -> aux (a :: acc) t
+  in aux [] lst;;
+  
+(* Actual solution *)
+let flatten list =
+  let rec aux acc = function
+    | [] -> acc
+    | One x :: t -> aux (x :: acc) t
+    | Many l :: t -> aux (aux acc l) t 
+in List.rev (aux [] list);;
+
+let () = List.iter (Printf.printf "%s, ") (flatten [ One "a" ; Many [ One "b" ; Many [ One "c" ; One "d" ] ; One "e" ] ])
