4940be2e4212c653d497fdc8207bc3d2a6703259
Rawley Fowler <rawleyfowler@gmail.com>
2022-10-04 18:16:13 -0600

    simplify replicate

--- 15_replicate_n_times.ml
+++ 15_replicate_n_times.ml
@@ -6,10 +6,10 @@ let rec replicate =
     let rec aux acc c n =
       if n > 0 then aux (c :: acc) h (pred n) 
       else acc
-    in fun n -> (aux [] h n) @ (replicate t n)
+    in fun n -> (aux (replicate t n) h n)
 
 (* Actual solution *)
-let replicate list n =
+let replicate' list n =
   let rec prepend n acc x =
     if n = 0 then acc else prepend (n-1) (x :: acc) x in
   let rec aux acc = function
